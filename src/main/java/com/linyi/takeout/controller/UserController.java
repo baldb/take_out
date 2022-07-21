7927208e14079e7d382858ed0a211d3c97f9a6c7
linyi <704600358@qq.com>
2022-07-21 14:46:11 +0800

    用户端的退出功能接口

--- src/main/java/com/linyi/takeout/controller/UserController.java
+++ src/main/java/com/linyi/takeout/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -100,5 +101,18 @@ public class UserController {
 
         log.info("验证码错误，请重新输入～");
         return R.error("登录失败");
+    }
+
+
+    /**
+     * 退出登录，移除session
+     * http://localhost:8080/user/loginout
+     * @param request
+     * @return
+     */
+    @PostMapping("/loginout")
+    public R<String> logout(HttpServletRequest request) {
+        request.getSession().removeAttribute("user");
+        return R.success("退出成功");
     }
 }
